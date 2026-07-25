7cfc2899914fb8f901228aba9376d1a560e3efab
Ayush <aksrivastav0825@gmail.com>
2026-07-25 14:07:41 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
-    'https://cravely-food-delivery-website-ldqsuw9rg.vercel.app',  // ✅ ADD THIS
+    'https://cravely-food-delivery-website.vercel.app/',  // ✅ ADD THIS
     'https://cravely-backend-dmak.onrender.com'
 ]
 
